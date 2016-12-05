ccd2cfbb8d5cd416d872814d769df27016c2d018
Breno Zeymer <bzeymer@gmail.com>
2016-12-05 11:04:21 -0200

    Finalizada etapa do curso de react-redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+
+    return [
+        {title: 'Javascript', author: 'Fulano de Tal', pages: 215},
+        {title: 'HTML5', author: 'Ciclano Silva', pages: 542},
+        {title: 'Java', author: 'Herculano Freitas', pages: 321},
+        {title: 'Python', author: 'Fulano Souza', pages: 955},
+        {title: 'Ruby', author: 'Ciclano Farias', pages: 125}
+    ];
+}
